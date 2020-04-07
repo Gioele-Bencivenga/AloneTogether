03ce23332701fa0b1acc2ea1dcfd8ea76bf012ca
Gioele <gioelebenci@live.com>
2020-04-07 17:36:29 +0100

    Coins, spritesheets
    Finished converting the character images from folders of single images to spritesheets.
    
    Created coins that tween out of existence when walked over.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,15 +1,17 @@
 package;
 
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxCamera.FlxCameraFollowStyle;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.tile.FlxTilemap;
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
 import flixel.FlxState;
-import source.myClasses.*;
+import myClasses.*;
 
 class PlayState extends FlxState {
 	var player:Player;
+	var coins:FlxTypedGroup<Coin>;
 
 	var map:FlxOgmo3Loader;
 
@@ -19,10 +21,9 @@ class PlayState extends FlxState {
 	var rooftopsLayer:FlxTilemap;
 
 	override public function create():Void {
-
 		/// TILEMAP STUFF
 		map = new FlxOgmo3Loader(AssetPaths.cityTilemap__ogmo, AssetPaths.city1__json);
-		collisionsLayer = map.loadTilemap(AssetPaths.tiles__png, "walls");
+		collisionsLayer = map.loadTilemap(AssetPaths.tiles__png, "collisions");
 		collisionsLayer.setTileProperties(1, FlxObject.NONE);
 		collisionsLayer.setTileProperties(2, FlxObject.ANY);
 		add(collisionsLayer);
@@ -30,7 +31,11 @@ class PlayState extends FlxState {
 		add(groundLayer);
 		buildingsLayer = map.loadTilemap(AssetPaths.tilemap_packed__png, "buildings");
 		add(buildingsLayer);
-		
+
+		/// COIN STUFF
+		coins = new FlxTypedGroup<Coin>();
+		add(coins);
+
 		/// PLAYER STUFF
 		player = new Player();
 		map.loadEntities(placeEntities, "entities");
@@ -39,7 +44,7 @@ class PlayState extends FlxState {
 		// we put the rooftops after the player so they get rendered in front of it
 		rooftopsLayer = map.loadTilemap(AssetPaths.tilemap_packed__png, "rooftops");
 		add(rooftopsLayer);
-		
+
 		/// CAMERA STUFF
 		collisionsLayer.follow(); // lock the camera to the wall map edges
 		FlxG.camera.follow(player, FlxCameraFollowStyle.LOCKON);
@@ -52,11 +57,21 @@ class PlayState extends FlxState {
 		super.update(elapsed);
 
 		FlxG.collide(player, collisionsLayer);
+		FlxG.overlap(player, coins, playerTouchCoin);
 	}
 
 	function placeEntities(entity:EntityData) {
 		if (entity.name == "player") {
 			player.setPosition(entity.x, entity.y);
+		} else if (entity.name == "coin") {
+			coins.add(new Coin(entity.x + 4,
+				entity.y + 4)); // we add the coin in the ogmo project to our coins group, the +4 is to center the coin in the middle of the tile
+		}
+	}
+
+	function playerTouchCoin(player:Player, coin:Coin) {
+		if (player.alive && player.exists && coin.alive && coin.exists) {
+			coin.kill();
 		}
 	}
 }
--- source/myClasses/Coin.hx
+++ source/myClasses/Coin.hx
@@ -1,10 +1,22 @@
 package myClasses;
 
+import flixel.FlxG;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.FlxSprite;
 
 class Coin extends FlxSprite {
 	public function new(x:Float, y:Float) {
 		super(x, y);
-		//loadGraphic(Assetp, false, 8, 8);
+		loadGraphic(AssetPaths.coin__png, false, 8, 8);
+	}
+
+	override function kill() {
+		alive = false;
+
+		var randX = FlxG.random.int(-10, 10);
+		var randY = FlxG.random.int(10, 20);
+
+		FlxTween.tween(this, {alpha: 0, x: x + randX, y: y - randY}, 0.50, {ease: FlxEase.circOut, onComplete: function(_) exists = false});
 	}
 }
